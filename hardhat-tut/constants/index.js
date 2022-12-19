54a84fa327b3d75f825fc8bdc7ec3c80506fa30a
root <root@LAPTOP-SHMBG1K8.localdomain>
2022-12-19 14:45:36 +0530

    update metadata url

--- hardhat-tut/constants/index.js
+++ hardhat-tut/constants/index.js
@@ -1,4 +1,4 @@
 const WHITELIST_CONTRACT_ADDRESS = "0xE7ae8330CE0c330b4D80eE15E0AA438bB97A88ca";
-const METADATA_URL = "";
+const METADATA_URL = "https://nft-collection-1-rj5t.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
